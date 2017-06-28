e20f7b9be42b2d1dd9a0b637a20747484f5153ad
Paweł Gesek <pgesek@soldevelo.com>
2017-06-28 13:17:02 +0200

    Fix metadata util

--- api/src/main/java/org/openmrs/module/xdssender/XdsSenderConstants.java
+++ api/src/main/java/org/openmrs/module/xdssender/XdsSenderConstants.java
@@ -6,7 +6,22 @@ public final class XdsSenderConstants {
     public static final String ATTRIBUTE_NAME_TELECOM = "Telecom";
     public static final String ATTRIBUTE_NAME_EXTERNAL_ID = "ExternalId";
     public static final String ATTRIBUTE_NAME_CIVIL_STATUS = "Civil Status";
+
+    public static final String CODE_SYSTEM_LOINC = "2.16.840.1.113883.6.1";
+    public static final String CODE_SYSTEM_NAME_LOINC = "LOINC";
+    public static final String CODE_SYSTEM_SNOMED = "2.16.840.1.113883.6.96";
+    public static final String CODE_SYSTEM_ACT_CODE = "2.16.840.1.113883.5.4";
+    public static final String CODE_SYSTEM_OBSERVATION_VALUE = "2.16.840.1.113883.5.1063";
+    public static final String CODE_SYSTEM_FAMILY_MEMBER = "2.16.840.1.113883.5.111";
+    public static final String CODE_SYSTEM_CIEL = "9.9.9.9.9.9.9"; // TODO: Get the actual OID for this
+    public static final String CODE_SYSTEM_UCUM = "2.16.840.1.113883.6.8";
+    public static final String CODE_SYSTEM_ROUTE_OF_ADMINISTRATION = "2.16.840.1.113883.5.112";
     public static final String CODE_SYSTEM_MARITAL_STATUS = "2.16.840.1.113883.5.2";
+    public static final String CODE_SYSTEM_CVX = "2.16.840.1.113883.6.59";
+    public static final String CODE_SYSTEM_RXNORM = "2.16.840.1.113883.6.88";
+    public static final String CODE_SYSTEM_IHE_ACT_CODE = "1.3.6.1.4.1.19376.1.5.3.2";
+    public static final String CODE_SYSTEM_CPT4 = "2.16.840.1.113883.6.12";
+    public static final String CODE_SYSTEM_ICD_10 = "2.16.840.1.113883.6.3";
 
     private XdsSenderConstants() {
     }
