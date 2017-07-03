875c7e92baf55a3fd72d1f016c75e7f74b541b10
Michal Kus <mkus@soldevelo.com>
2017-07-03 10:53:08 +0200

    #131 Added sending XDS document after creating Encounter

--- api/src/main/java/org/openmrs/module/xdssender/api/cda/EncounterEventListener.java
+++ api/src/main/java/org/openmrs/module/xdssender/api/cda/EncounterEventListener.java
@@ -1,13 +1,20 @@
 package org.openmrs.module.xdssender.api.cda;
 
+import org.openmrs.Encounter;
+import org.openmrs.api.context.Context;
 import org.openmrs.event.Event;
 import org.openmrs.event.EventListener;
+import org.openmrs.module.xdssender.api.service.XdsExportSerivce;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.jms.JMSException;
 import javax.jms.MapMessage;
 import javax.jms.Message;
 
 public class EncounterEventListener implements EventListener {
+
+	@Autowired
+	private XdsExportSerivce exportSerivce;
 	
 	@Override
 	public void onMessage(Message message) {
@@ -15,6 +22,8 @@ public class EncounterEventListener implements EventListener {
 			MapMessage mapMessage = (MapMessage) message;
 			if (Event.Action.CREATED.toString().equals(mapMessage.getString("action"))) {
 				String uuid = ((MapMessage) message).getString("uuid");
+				Encounter encounter = Context.getEncounterService().getEncounterByUuid(uuid);
+				exportSerivce.exportProvideAndRegister(encounter, encounter.getPatient());
 			}
 		}
 		catch (JMSException e) {
