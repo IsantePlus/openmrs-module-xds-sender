2c7639519f87b82b5806c7b21cff4e05ce3ee338
Wojciech Buława <wbulawa@soldevelo.com>
2018-07-16 12:18:28 +0200

    ILI-28: Added send condition for SCC lab orders

--- api/src/main/java/org/openmrs/module/xdssender/api/cda/ORM_O01DocumentBuilder.java
+++ api/src/main/java/org/openmrs/module/xdssender/api/cda/ORM_O01DocumentBuilder.java
@@ -17,10 +17,10 @@ public class ORM_O01DocumentBuilder {
 
     private final Log log = LogFactory.getLog(this.getClass());
 
-    private static final String FORMAT_CODE= "HL7/Lab 2.5";
+    private static final String FORMAT_CODE = "HL7/Lab 2.5";
 
     // @see https://wiki.ihe.net/index.php/XDS_classCode_Metadata_Coding_System
-    private static final String CLASS_CODE= "PRESCRIPTIONS";
+    private static final String CLASS_CODE = "PRESCRIPTIONS";
     private static final String CLASS_CODE_SCHEME = "XDS Class Code";
 
     @Autowired
@@ -39,13 +39,15 @@ public class ORM_O01DocumentBuilder {
     }
 
     public DocumentModel buildDocument(Encounter encounter) {
-        try {
-            String message = orderGeneratorManager.generateORMO01Message(encounter, OrderDestination.SCC);
-            byte[] data = message.getBytes(StandardCharsets.UTF_8);
+        if (OrderDestination.searchForExistence(encounter, OrderDestination.SCC)) {
+            try {
+                String message = orderGeneratorManager.generateORMO01Message(encounter, OrderDestination.SCC);
+                byte[] data = message.getBytes(StandardCharsets.UTF_8);
 
-            return DocumentModel.createInstance(data, getClassCode(), getClassCodeScheme(), getFormatCode(), message);
-        } catch (MessageCreationException ex) {
-            log.error("Error generating orders:", ex);
+                return DocumentModel.createInstance(data, getClassCode(), getClassCodeScheme(), getFormatCode(), message);
+            } catch (MessageCreationException ex) {
+                log.error("Error generating orders:", ex);
+            }
         }
 
         return null;
