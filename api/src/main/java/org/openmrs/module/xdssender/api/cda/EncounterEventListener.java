0afc3f85f72f6b4addba8a9951908c63822b25e4
moshonk <omondi.ken@gmail.com>
2021-02-20 13:46:58 +0300

    Replaced invalid characters in the Config file

--- api/src/main/java/org/openmrs/module/xdssender/api/cda/EncounterEventListener.java
+++ api/src/main/java/org/openmrs/module/xdssender/api/cda/EncounterEventListener.java
@@ -1,5 +1,13 @@
 package org.openmrs.module.xdssender.api.cda;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+import javax.jms.Message;
+
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.openmrs.Encounter;
@@ -18,22 +26,21 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.jms.JMSException;
-import javax.jms.MapMessage;
-import javax.jms.Message;
-import java.io.IOException;
-
 @Component("xdssender.EncounterEventListener")
 public class EncounterEventListener implements EventListener {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(EncounterEventListener.class);
-
+	
+	private static final String WILDCARD_MATCH = "ALL";
+	
+	private static final String PROP_ENCOUNTER_TYPE_TO_PROCESS = "xdssender.encounterTypesToProcess";
+	
 	@Autowired
 	private XdsSenderConfig config;
-
+	
 	@Autowired
 	private PatientEcidUpdater ecidUpdater;
-
+	
 	@Override
 	public void onMessage(Message message) {
 		try {
@@ -45,36 +52,36 @@ public class EncounterEventListener implements EventListener {
 			
 			if (Event.Action.CREATED.toString().equals(messageAction)
 			        || Event.Action.UPDATED.toString().equals(messageAction)) {
+				
 				String uuid = ((MapMessage) message).getString("uuid");
-				Encounter encounter = Context.getEncounterService().getEncounterByUuid(uuid);
-				if (encounter.getForm() == null) {
-					LOGGER.warn("Skipped sending Encounter %s (formId is NULL "
-							+ "-> probably it's the creating encounter)");
-				} else {
-					Patient patient = Context.getPatientService()
-							.getPatient(encounter.getPatient().getPatientId());
-
-					ecidUpdater.fetchEcidIfRequired(patient);
-
-					XdsExportService service = Context.getService(XdsExportService.class);
-
-					try {
-						service.exportProvideAndRegister(encounter, patient);
-					} catch (Exception e) {
-						ErrorHandlingService errorHandler = config.getXdsBErrorHandlingService();
-						if (errorHandler != null) {
-							LOGGER.error("XDS export exception occurred", e);
-							errorHandler.handle(
-									prepareParameters(encounter, patient),
-									XdsBErrorHandlingService.EXPORT_PROVIDE_AND_REGISTER_DESTINATION,
-									true,
-									ExceptionUtils.getFullStackTrace(e));
+				List<String> encounterTypesToProcess = null;
+				String propEncounterTypesToProcess = Context.getAdministrationService()
+				        .getGlobalProperty(PROP_ENCOUNTER_TYPE_TO_PROCESS);
+				
+				if (propEncounterTypesToProcess != null) {
+					
+					encounterTypesToProcess = Arrays.asList(propEncounterTypesToProcess.split(WILDCARD_MATCH));
+					if (encounterTypesToProcess.size() > 0) {
+						if (encounterTypesToProcess.contains(WILDCARD_MATCH)) {
+							exportEncounter(uuid);
+							
 						} else {
-							throw new RuntimeException("XDS export exception occurred "
-									+ "with not configured XDS.b error handler", e);
+							LOGGER.debug("Found {} encounter types to filter from global config",
+							    encounterTypesToProcess.size());
+							for (String encounterUUID : encounterTypesToProcess) {
+								LOGGER.debug("Matching encounter types to send XDS repository: {}", uuid);
+								if (encounterUUID.equals(uuid)) {
+									exportEncounter(uuid);
+									
+								}
+							}
 						}
+					} else {
+						exportEncounter(uuid);
+						
 					}
 				}
+				
 			}
 			
 			Context.closeSession();
@@ -84,12 +91,42 @@ public class EncounterEventListener implements EventListener {
 		}
 	}
 	
+	private void exportEncounter(String encounterUuid) {
+		Encounter encounter = Context.getEncounterService().getEncounterByUuid(encounterUuid);
+		if (encounter.getForm() == null) {
+			LOGGER.warn("Skipped sending Encounter %s (formId is NULL " + "-> probably it's the creating encounter)");
+		} else {
+			Patient patient = Context.getPatientService().getPatient(encounter.getPatient().getPatientId());
+			
+			ecidUpdater.fetchEcidIfRequired(patient);
+			
+			XdsExportService service = Context.getService(XdsExportService.class);
+			
+			try {
+				service.exportProvideAndRegister(encounter, patient);
+			}
+			catch (Exception e) {
+				ErrorHandlingService errorHandler = config.getXdsBErrorHandlingService();
+				if (errorHandler != null) {
+					LOGGER.error("XDS export exception occurred", e);
+					errorHandler.handle(prepareParameters(encounter, patient),
+					    XdsBErrorHandlingService.EXPORT_PROVIDE_AND_REGISTER_DESTINATION, true,
+					    ExceptionUtils.getFullStackTrace(e));
+				} else {
+					throw new RuntimeException("XDS export exception occurred " + "with not configured XDS.b error handler",
+					        e);
+				}
+			}
+		}
+	}
+	
 	private String prepareParameters(Encounter encounter, Patient patient) {
-		ExportProvideAndRegisterParameters parameters =
-				new ExportProvideAndRegisterParameters(patient.getUuid(), encounter.getUuid());
+		ExportProvideAndRegisterParameters parameters = new ExportProvideAndRegisterParameters(patient.getUuid(),
+		        encounter.getUuid());
 		try {
 			return new ObjectMapper().writeValueAsString(parameters);
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			throw new RuntimeException("Cannot prepare parameters for OutgoingMessageException", e);
 		}
 	}
