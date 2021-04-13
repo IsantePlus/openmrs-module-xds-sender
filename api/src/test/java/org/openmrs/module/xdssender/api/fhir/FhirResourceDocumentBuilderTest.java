e760199e1ed901480521635f2a28fb57204382d9
Ken Omondi <omondi.ken@gmail.com>
2021-04-13 16:44:38 +0300

    49 add fhir resource bundle to cda doc (#57)
    * Added debugging messages
    
    * Restored ability to attach documents to SOAP requests
    
    * Fixed HL7 parsing error resulting from additional time parameter on the birthdate
    
    * Added provision to filter getMessages request by facility sitecode
    
    * Code formatting changes
    
    * Removed unused unit test
    
    * Replaced invalid characters in the Config file
    
    * COde formatting changes
    
    * Added method override to attach more than 1 additional documents
    
    * Added method override to attach more than 1 additional documents
    
    * Added method override to attach more than 1 additional documents
    
    * Added code stubs for the fhir document builder
    
    * Added jnuit & upgraded maven jaxb version
    
    * Added unit tests
    
    * Code clean-up
    
    * Fixing a failing unit test
    
    * Fixed failing unit tests
    
    * Added a System Identifier to the Patient Resource

---
+++ api/src/test/java/org/openmrs/module/xdssender/api/fhir/FhirResourceDocumentBuilderTest.java
@@ -0,0 +1,34 @@
+package org.openmrs.module.xdssender.api.fhir;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openmrs.Encounter;
+import org.openmrs.Patient;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.xdssender.api.cda.model.DocumentModel;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import static org.junit.Assert.assertNotNull;
+
+@ContextConfiguration(locations = { "classpath*:moduleApplicationContext.xml", "classpath*:applicationContext-service.xml" }, inheritLocations = false)
+public class FhirResourceDocumentBuilderTest extends BaseModuleContextSensitiveTest {
+
+    private static final String DATASET = "lab-dataset.xml";
+    private static final String GLOBAL_CONFIGS = "global-configs.xml";
+
+    @Before
+    public void setUp() throws Exception {
+        executeDataSet(DATASET);
+        executeDataSet(GLOBAL_CONFIGS);
+    }
+
+    @Test
+    public void testBuildDocument() {
+        Patient patient = Context.getPatientService().getPatient(10);
+        Encounter encounter = Context.getEncounterService().getEncounter(21);
+        FhirResourceDocumentBuilder fhirResourceDocumentBuilder = Context.getService(FhirResourceDocumentBuilder.class);
+        DocumentModel dm = fhirResourceDocumentBuilder.buildDocument(patient, encounter);
+        assertNotNull(dm);
+    }
+}
