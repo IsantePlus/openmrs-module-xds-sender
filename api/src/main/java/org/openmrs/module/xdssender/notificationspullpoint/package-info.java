5acce21cadd326f41ebb427d3dab5f1a0a25caf9
Ian <ian.c.bacher@gmail.com>
2025-07-09 11:18:36 -0400

    Fix for namespace not being set for all classes

--- api/src/main/java/org/openmrs/module/xdssender/notificationspullpoint/package-info.java
+++ api/src/main/java/org/openmrs/module/xdssender/notificationspullpoint/package-info.java
@@ -1,3 +1,4 @@
+@XmlSchema(namespace="http://docs.oasis-open.org/wsn/b-2", elementFormDefault=XmlNsForm.QUALIFIED)
 package org.openmrs.module.xdssender.notificationspullpoint;
 
 import javax.xml.bind.annotation.XmlNsForm;
